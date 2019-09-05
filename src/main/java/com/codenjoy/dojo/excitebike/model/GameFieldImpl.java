76c2a5dc9df2e2e21371c81673e64bceee75b6a2
pabob <Pavel_Bobylev@epam.com>
2019-09-05 13:07:57 +0600

    check if current cell is free during respawn #2

--- src/main/java/com/codenjoy/dojo/excitebike/model/GameFieldImpl.java
+++ src/main/java/com/codenjoy/dojo/excitebike/model/GameFieldImpl.java
@@ -232,6 +232,7 @@ public class GameFieldImpl implements GameField {
     private boolean isFree(Point point) {
         Point nextPoint = new PointImpl(point.getX()+1, point.getY());
         return !getAliveBikes().contains(point)
+                && !fences.contains(point)
                 && !fences.contains(nextPoint)
                 && !allShiftableElements.get(OBSTACLE).contains(point)
                 && !allShiftableElements.get(OBSTACLE).contains(nextPoint)
